0b633f037166e3af09f9db31ddd346682e867f15
KhanhHoa212 <khanhhoakt2k4@gmail.com>
2026-03-04 08:58:57 +0700

    add Semeter and interface

---
+++ GPMS.Application/Interfaces/Repositories/ISemesterRepository.cs
@@ -0,0 +1,17 @@
+using GPMS.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GPMS.Application.Interfaces.Repositories;
+
+public interface ISemesterRepository
+{
+    Task<Semester?> GetByIdAsync(int semesterId);
+    Task<Semester?> GetByCodeAsync(string semesterCode);
+    Task<IEnumerable<Semester>> GetAllAsync();
+    Task AddAsync(Semester semester);
+    Task UpdateAsync(Semester semester);
+    Task DeleteAsync(int semesterId);
+    Task<bool> ExistsAsync(int semesterId);
+    Task SaveChangesAsync();
+}
---
+++ GPMS.Infrastructure/Repositories/SemesterRepository.cs
@@ -0,0 +1,35 @@
+using GPMS.Domain.Entities;
+using GPMS.Application.Interfaces.Repositories;
+using GPMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GPMS.Infrastructure.Repositories;
+
+public class SemesterRepository : ISemesterRepository
+{
+    private readonly GpmsDbContext _context;
+    public SemesterRepository(GpmsDbContext context) => _context = context;
+
+    public async Task<Semester?> GetByIdAsync(int semesterId) => 
+        await _context.Semesters.Include(s => s.Projects).FirstOrDefaultAsync(s => s.SemesterID == semesterId);
+
+    public async Task<Semester?> GetByCodeAsync(string semesterCode) => 
+        await _context.Semesters.FirstOrDefaultAsync(s => s.SemesterCode == semesterCode);
+
+    public async Task<IEnumerable<Semester>> GetAllAsync() => 
+        await _context.Semesters.Include(s => s.Projects).ToListAsync();
+
+    public async Task AddAsync(Semester semester) => await _context.Semesters.AddAsync(semester);
+    public async Task UpdateAsync(Semester semester) => _context.Semesters.Update(semester);
+    
+    public async Task DeleteAsync(int semesterId)
+    {
+        var semester = await _context.Semesters.FindAsync(semesterId);
+        if (semester != null) _context.Semesters.Remove(semester);
+    }
+
+    public async Task<bool> ExistsAsync(int semesterId) => await _context.Semesters.AnyAsync(s => s.SemesterID == semesterId);
+    public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+}
